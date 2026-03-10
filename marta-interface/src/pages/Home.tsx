0c1aa4a773d0559231a3fe4144b938b1f3db1eb0
Guillermo-Reyes1 <reyesguillermo2305@gmail.com>
2026-03-10 18:23:30 -0400

    Home.tsx skeleton

--- marta-interface/src/pages/Home.tsx
+++ marta-interface/src/pages/Home.tsx
@@ -1,3 +1,30 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
-    return <h1>Home Page</h1>;
-  }
+  return (
+    <div>
+      <h1>MARTA</h1>
+
+      <nav>
+        <Link to="/about">About MARTA</Link>
+      </nav>
+
+      <h2>View Route Schedule</h2>
+
+      <ul>
+        <li>
+          <Link to="/lines/gold">Gold Line</Link>
+        </li>
+        <li>
+          <Link to="/lines/red">Red Line</Link>
+        </li>
+        <li>
+          <Link to="/lines/green">Green Line</Link>
+        </li>
+        <li>
+          <Link to="/lines/blue">Blue Line</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
